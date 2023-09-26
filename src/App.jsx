04e45740dd696e57c45daceb164f99ab08989c57
Bernardo-Mattos-HomePc <bernardomtt2@gmail.com>
2023-09-26 00:07:11 -0300

    adicionei o sobre, seus ruins

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Participantes from './components/participantes';
+import Sobre from './components/Sobre/sobre';
 
 function App() {
   return (
-    <div>
+    <div className='father'>
       <Header />
       <Participantes/> 
+      <Sobre /> 
     </div>
   );
 }
---
+++ src/components/Sobre/sobre.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './sobre.modules.css'
+
+export default function Sobre() {
+  return (
+    <div className={styles.containerSobre}> 
+      <h1>
+        sim porra
+      </h1>
+    </div>
+  )
+}
